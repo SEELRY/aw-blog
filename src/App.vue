9a7d77b4698eb11ad3d90845bab3575f281da951
Arthur <wang@xunnn.com>
2019-05-05 21:57:24 +0800

    1.添加页面样式 2.添加文章列表请并求数据

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,30 @@
 <template>
   <div id="app">
-    <add-blog></add-blog>
+    <!-- <add-blog></add-blog> -->
+    <show-blogs></show-blogs>
   </div>
 </template>
 
 <script>
 import AddBlog from './components/AddBlog'
+import ShowBlogs from './components/ShowBlogs'
 
 export default {
   name: 'App',
   components: {
-    AddBlog
+    AddBlog,
+    ShowBlogs
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
